063cffe7437ed8c617efb2f36ed5be1f01b56f0f
Samuele-Chioda <samuelechioda@gmail.com>
2024-07-15 17:16:48 +0200

    scaffolding completo

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import 'resurces/scss/app.scss';
+const path = require('path');
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                // Modifichiamo il percorso del css usando sass
+                'resources/scss/app.scss',
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
     ],
+    //Aggiungiamo un alias per la cartella /resources/
+    resolve: {
+        alias: {
+            '~resources': '/resources/',
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    },
 });
